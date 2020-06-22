f9fe61c641adb021010a5e27cc79965e26428f28
jezzicaaaaa <jezzicamae@gmail.com>
2020-06-22 18:53:53 -0400

    redux persist integration to store cart items

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,22 @@
 // All the reducers that we will be writing will go here.
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //actual local storage object on our window browser
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
-export default combineReducers({
+//persist config
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: [ 'cart' ]
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
-	cart: cartReducer
+	cart: cartReducer,
+	directory: directoryReducer
 });
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 //debugger for redux code
 import logger from 'redux-logger';
 
@@ -7,6 +8,8 @@ import rootReducer from './root-reducer';
 // middleware that the store is expecting from redux will be an array
 const middlewares = [ logger ];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store); //persisted version of the store
+
+export default { store, persistor };
